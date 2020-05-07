bf9cab484e87fcccc6618f375583a6d68bcb9bd1
Tamminedi <VTAMMINE@ford.com>
2020-05-06 22:15:44 -0400

    No Types needed: class-transformer

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,22 @@
-import _ from 'lodash';
+import 'reflect-metadata';
+import { plainToClass} from "class-transformer";
+import {Product} from "./product.model";
 
-declare var GLOBAL:any;
+const products = [
+    { title: 'A Carpet', price: 29.99},
+    { title: 'A Book', price: 10.99}
+];
 
-console.log(_.shuffle([1,2,3]));
-console.log(GLOBAL);
+/*const loadedProducts = products.map(prod => {
+    return new Product(prod.title, prod.price);
+});*/
+
+const loadedProducts = plainToClass(Product, products);
+
+for(const prod of loadedProducts) {
+    console.log(prod.getInformation());
+}
+
+// const p1 = new Product('A Book', 12.99);
+
+//console.log(p1.getInformation());
